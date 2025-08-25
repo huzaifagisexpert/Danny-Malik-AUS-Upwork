0ed0d568c0fd5f3594336036828666f0e93496f2
Micknesso-Hub <huzaifaumair471@gmail.com>
2025-08-25 22:33:30 +0500

    app.py

--- app.py
+++ app.py
@@ -86,3 +86,14 @@ if uploaded_file is not None:
 
     except Exception as e:
         st.error(f"⚠️ Error: {e}")
+
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
